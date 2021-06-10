7f27cfe31a64e64d3cf6674928a577601b411ae8
clj-jackson <74957741+clj-jackson@users.noreply.github.com>
2021-06-10 11:24:27 +0100

    Update medianoftwosortedarrays.py

--- medianoftwosortedarrays.py
+++ medianoftwosortedarrays.py
@@ -1,3 +1,6 @@
+# Leetcode 4. Median of Two Sorted Arrays
+# Difficulty - Hard
+
 """
 Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
 
